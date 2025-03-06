3c2c746a1adb4c299adff68d344b96cdbe7e4a08
PashaShoshyn <155258077+PashaShoshyn@users.noreply.github.com>
2025-03-06 19:09:16 +0200

    hgjgj

--- js/main.js
+++ js/main.js
@@ -1,158 +1,74 @@
-// Завдання 1
-function Account({ login, email }) {
-    this.login = login;
-    this.email = email;
+// const targetBtn = document.querySelector('.js-target-btn')
+// const addBtn = document.querySelector('.js-add-btn')
+// const removeBtn = document.querySelector('.js-remove-btn')
+
+// const click = () => {
+//     console.log('click');
+// }
+
+// addBtn.addEventListener('click', () => {
+//     console.log('add click even');
+//     targetBtn.addEventListener('click', click);
+// });
+
+// removeBtn.addEventListener('click', () => {
+//     console.log('remove click even');
+//     targetBtn.removeEventListener('click', click);
+// })
+
+
+
+// const mouseBox = document.getElementById('boxMouse');
+
+// mouseBox.addEventListener('mousedown', () => mouseBox.style.backgroundColor = 'blue');
+// mouseBox.addEventListener('mouseup', () => mouseBox.style.backgroundColor = 'lightblue');
+// mouseBox.addEventListener('mouseover', () => mouseBox.style.backgroundColor = 'green');
+// mouseBox.addEventListener('mouseout', () => mouseBox.style.backgroundColor = 'lightgreen');
+
+// mouseBox.addEventListener('mousemove', (event) => {
+//     console.log(`X: ${event.clientX}, Y: ${event.clientY}`);
+// });
+
+
+
+// const coords = document.getElementById('coords');
+
+// mouseBox.addEventListener('mousemove', (event) => {
+//     coords.textContent = `Координати: X: ${event.clientX}, Y: ${event.clientY}`;
+// });
+
+
+
+
+const box = document.getElementById('box');
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'y') {
+        box.style.background = 'yellow';
+    } else if (event.key === 'g') {
+        box.style.background = 'green';
+    }
+});
+
+// Гра "Натисни правильну клавішу"
+const keys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+const keyDisplay = document.getElementById('key');
+const message = document.getElementById('message');
+let currentKey = '';
+
+function startNewGame() {
+    currentKey = keys[Math.floor(Math.random() * keys.length)];
+    keyDisplay.textContent = currentKey;
+    message.textContent = 'Натисни правильну клавішу!';
 }
 
-Account.prototype.getInfo = function () {
-    console.log(`Login: ${this.login}, Email: ${this.email}`);
-};
-
-const mango = new Account({ login: 'Mangozedog', email: 'mango@dog.woof' });
-mango.getInfo();
-
-const poly = new Account({ login: 'Poly', email: 'poly@mail.com' });
-poly.getInfo();
-
-// Завдання 2
-class User {
-    constructor({ name, age, followers }) {
-        this.name = name;
-        this.age = age;
-        this.followers = followers;
+document.addEventListener('keydown', (event) => {
+    if (event.key === currentKey) {
+        message.textContent = 'Правильно!';
+    } else {
+        message.textContent = 'Спробуй ще раз!';
     }
+});
 
-    getInfo() {
-        console.log(`User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
-    }
-}
-
-const user1 = new User({ name: 'Mango', age: 2, followers: 20 });
-user1.getInfo();
-
-const user2 = new User({ name: 'Poly', age: 3, followers: 17 });
-user2.getInfo();
-
-// Завдання 3
-class Storage {
-    constructor(items) {
-        this.items = items;
-    }
-
-    getItems() {
-        return this.items;
-    }
-
-    addItem(item) {
-        this.items.push(item);
-    }
-
-    removeItem(item) {
-        this.items = this.items.filter(i => i !== item);
-    }
-}
-
-const storage = new Storage(["Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор"]);
-console.table(storage.getItems());
-storage.addItem("Дроїд");
-console.table(storage.items);
-storage.removeItem("Пролонгер");
-console.table(storage.items);
-
-// Завдання 4
-class StringBuilder {
-    constructor(value) {
-        this._value = value;
-    }
-
-    get value() {
-        return this._value;
-    }
-
-    append(str) {
-        this._value += str;
-    }
-
-    prepend(str) {
-        this._value = str + this._value;
-    }
-
-    pad(str) {
-        this._value = str + this._value + str;
-    }
-}
-
-const builder = new StringBuilder(".");
-builder.append("^");
-console.log(builder.value);
-builder.prepend("^");
-console.log(builder.value);
-builder.pad("=");
-console.log(builder.value);
-
-// Завдання 5
-class Car {
-    constructor({ maxSpeed, price }) {
-        this.speed = 0;
-        this.price = price;
-        this.maxSpeed = maxSpeed;
-        this.isOn = false;
-        this.distance = 0;
-    }
-
-    static getSpecs(car) {
-        console.log(`maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`);
-    }
-
-    get price() {
-        return this._price;
-    }
-
-    set price(newPrice) {
-        this._price = newPrice;
-    }
-
-    turnOn() {
-        this.isOn = true;
-    }
-
-    turnOff() {
-        this.isOn = false;
-        this.speed = 0;
-    }
-
-    accelerate(value) {
-        if (this.speed + value <= this.maxSpeed) {
-            this.speed += value;
-        }
-    }
-
-    decelerate(value) {
-        if (this.speed - value >= 0) {
-            this.speed -= value;
-        }
-    }
-
-    drive(hours) {
-        if (this.isOn) {
-            this.distance += hours * this.speed;
-        }
-    }
-}
-
-const mustang = new Car({ maxSpeed: 200, price: 2000 });
-mustang.turnOn();
-mustang.accelerate(50);
-mustang.drive(2);
-Car.getSpecs(mustang);
-mustang.decelerate(20);
-mustang.drive(1);
-mustang.turnOff();
-Car.getSpecs(mustang);
-console.log(mustang.price);
-mustang.price = 4000;
-console.log(mustang.price);
-
-
-
-
+// Запуск гри при завантаженні сторінки
+startNewGame();
